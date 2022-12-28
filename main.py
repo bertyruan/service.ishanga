f8ac6aa09228c4ceab729129ee41d775b06c70c4
Albert Ruan <berty.ruan@gmail.com>
2022-12-28 18:51:21 +0530

    encapsulates window toggling

--- main.py
+++ main.py
@@ -8,24 +8,37 @@ ishanga = 'Ishanga Service:'
 # from kodi's internal system, the window id is 11200
 # ¯\_(ツ)_/¯
 screensaver_window = 1200
+video_window = 12005
 
+def activate_window(window_id):
+    if not xbmcgui.getCurrentWindowId() == window_id: 
+        xbmc.executebuiltin('Dialog.Close(all, true)')
+        xbmc.executebuiltin(f'ActivateWindow({window_id})')
 
 class XBMCPlayer(xbmc.Player):
     def onPlayBackStarted(self):
-        xbmc.log(f"{ishanga} PLAYBACK STARTED {xbmcgui.getCurrentWindowId()}", log_info)
-        xbmc.executebuiltin('Dialog.Close(all, true)')
-        xbmc.executebuiltin(f'ActivateWindow({screensaver_window})')
+        xbmc.log(f"{ishanga} playlist id {xbmc.PlayList(xbmc.PLAYLIST_VIDEO).getPlayListId()}", log_info)
+        xbmc.log(f"{ishanga} PLAYBACK STARTED", log_info)
+        activate_window(screensaver_window)
         xbmc.Player.pause(self)     
 
     def onPlayBackPaused(self):
-        xbmc.executebuiltin('Dialog.Close(all, true)')
-        xbmc.executebuiltin(f'ActivateWindow({screensaver_window})')
+        activate_window(screensaver_window)
         xbmc.log(f"{ishanga} VIDEO IS PAUSED. SCREENSAVER ON", log_info)
+
+    def onPlayBackResumed(self):
+        activate_window(video_window)
+
+    def onPlayBackSeek(self, time, seekOffset):
+        # xbmc.Player.pause(self)
+        xbmc.log(f"{ishanga} PLAYBACK SEEK", log_info)
+        activate_window(video_window)
 
         
 if __name__ == '__main__':
     xbmc.log(f"{ishanga} SUCCESSFULLY BOOTED", log_info)
     xbmc.executebuiltin('InhibitScreensaver(true)')
+    xbmc.executebuiltin('InhibitIdleShutdown(true)')
     
     player = XBMCPlayer()
     monitor = xbmc.Monitor()
