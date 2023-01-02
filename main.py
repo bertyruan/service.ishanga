6627bc13ee47ad39bddb584d997e1d6c635ca91f
Albert Ruan <berty.ruan@gmail.com>
2023-01-02 10:51:56 +0530

    refactors

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import xbmc
 import xbmcgui
+import sys 
 
-log_info = xbmc.LOGINFO
 ishanga = 'Ishanga Service:'
+# file:///D:/berty/isha-foundation/kodi/docs/html/df/dce/group__python__xbmcgui.html#gadb269db9ee11dfa03817f1585b0b1895
 
 # from my code's side the screensaver skin's window id is 1200
 # from kodi's internal system, the window id is 11200
 # ¯\_(ツ)_/¯
 screensaver_window = 1200
 video_window = 12005
+
+def log(file, text):
+    xbmc.log(f"{file}: {text}", xbmc.LOGINFO)
 
 def activate_window(window_id):
     if not xbmcgui.getCurrentWindowId() == window_id: 
@@ -17,31 +21,36 @@ def activate_window(window_id):
 
 class XBMCPlayer(xbmc.Player):
     def onPlayBackStarted(self):
-        xbmc.log(f"{ishanga} playlist id {xbmc.PlayList(xbmc.PLAYLIST_VIDEO).getPlayListId()}", log_info)
-        xbmc.log(f"{ishanga} PLAYBACK STARTED", log_info)
+        log(ishanga, f"playlist id {xbmc.PlayList(xbmc.PLAYLIST_VIDEO).getPlayListId()}") 
+        log(ishanga, "PLAYBACK STARTED")
         activate_window(screensaver_window)
         xbmc.Player.pause(self)     
 
     def onPlayBackPaused(self):
         activate_window(screensaver_window)
-        xbmc.log(f"{ishanga} VIDEO IS PAUSED. SCREENSAVER ON", log_info)
+        log(ishanga, "VIDEO IS PAUSED. SCREENSAVER ON")
 
     def onPlayBackResumed(self):
         activate_window(video_window)
 
     def onPlayBackSeek(self, time, seekOffset):
         # xbmc.Player.pause(self)
-        xbmc.log(f"{ishanga} PLAYBACK SEEK", log_info)
+        log(ishanga, "PLAYBACK SEEK")
         activate_window(video_window)
 
         
 if __name__ == '__main__':
-    xbmc.log(f"{ishanga} SUCCESSFULLY BOOTED", log_info)
+    log(ishanga,  "SUCCESSFULLY BOOTED")
     xbmc.executebuiltin('InhibitScreensaver(true)')
     xbmc.executebuiltin('InhibitIdleShutdown(true)')
     
+    # base_url = sys.argv[0]
+    # addon_handle = int(sys.argv[1])
+    # args =  sys.argv[2]
+    # log(ishanga, f"{base_url}, {addon_handle}, {args}")
+    log(ishanga, sys.argv)
     player = XBMCPlayer()
     monitor = xbmc.Monitor()
     while not monitor.abortRequested():
         if monitor.waitForAbort(1):
-            xbmc.log(f"{ishanga} SHUTTING DOWN", log_info)
+            log(ishanga, "SHUTTING DOWN")
--- video-keyboard.py
+++ video-keyboard.py
@@ -1,19 +1,22 @@
 import xbmc
 import xbmcgui
 
-log_info = xbmc.LOGINFO
+def log(file, text):
+    xbmc.log(f"{file}: {text}", xbmc.LOGINFO)
+
+
 keyboard_script = 'Ishanga Keyboard Script:'
 screensaver_window = 11200
 video_window = 12005
 video_nav_window = 10025
 
 if xbmc.Player().isPlayingVideo():
-    xbmc.log(f"{keyboard_script}: start", log_info)
+    log(keyboard_script, "STARTS")
     xbmc.executebuiltin('Dialog.Close(all, true)')
     xbmc.executebuiltin(f'ActivateWindow({video_window})')
     xbmc.Player().pause()
-    xbmc.log(f"{keyboard_script}: VIDEO IS_PLAYING {xbmc.Player().isPlaying()}", log_info)
+    log(keyboard_script, f"VIDEO IS_PLAYING {xbmc.Player().isPlaying()}")
 
 elif xbmcgui.getCurrentWindowId() == screensaver_window:
     xbmc.executebuiltin(f'ActivateWindow({video_nav_window})')
-    xbmc.log(f"{keyboard_script} window id is {xbmcgui.getCurrentWindowId()}", log_info)
+    log(keyboard_script, f"window id is {xbmcgui.getCurrentWindowId()}")
